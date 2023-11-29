43586ad5be65bb13091106d7f2522132348c2610
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-11-29 17:46:56 -0500

    Create gen_parentheses.py

---
+++ stack/gen_parentheses.py
@@ -0,0 +1,21 @@
+stack = []
+res = []
+n = 3
+
+
+def backtrack(openN, closedN):
+    if openN == closedN == n:
+        res.append("".join(stack))
+        return
+    if openN < n:
+        stack.append("(")
+        backtrack(openN + 1, closedN)
+        stack.pop()
+    if closedN < openN:
+        stack.append(")")
+        backtrack(openN, closedN + 1)
+        stack.pop()
+
+
+backtrack(0, 0)
+# return res

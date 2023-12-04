c85ff99b5506ee2f2de4f1fff753f9897a7d8832
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-12-04 16:09:22 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+# number = range(0, 9, 2)
+
+# for i in number:
+#     print(i)
+
+# list = [1, 2, 3, 4]
+# list[3] = 10
+# print(list)
+
+# list2 = [2, 45]
+# list3 = list + list2
+# print(list3)
+
+# list_string = "03/02/2008"
+# list_strip = list_string.split("/")
+# print(list_strip)
+
+# string = "hello"
+# string += " world"
+# print(string)
+
+relation = {"Jimmy": "brother"}
+print(relation["jimmy"])

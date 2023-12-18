5ef615c667cb3f8e019b688054e7fe37d408d7dc
Jadon Lama <Jadonlama357@gmail.com>
2023-12-18 16:36:41 -0500

    udpate

--- slidingWindow/dynamicwin.py
+++ slidingWindow/dynamicwin.py
@@ -1,10 +1,18 @@
 arr = [4, 2, 2, 7, 8, 1, 2, 8, 1, 0]
-tareget = 8
+target = 8
 
 
 def smallest_subarray(arr, target):
-    window_start = 0
-    window_end = len(arr) - 1
     min_window_size = len(arr) + 1
-    current_sum = 0
-    while ()
+    window_end = len(arr) - 1
+    window_start = 0
+    current_win_sum = 0
+    current_win_sum += min_window_size
+    while current_win_sum >= target:
+        min_window_size = min(min_window_size, window_end - window_start + 1)
+        current_win_sum -= arr[window_start]
+        window_start += 1
+    return min_window_size
+
+
+print(smallest_subarray(arr, target))

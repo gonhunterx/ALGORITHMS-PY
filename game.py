e5f13db150926baf361f0ddfbf975dc099d7d6b6
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-12-21 08:50:12 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,16 @@
+# Classes
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.gold = 0
+        self.inventory = {}
 
+    def display_stats(self):
+        print(self.name)
+        for item in self.inventory:
+            print(f"- {item.value()}")
+        print(self.gold)
+
+
+def main():
+    print("Welcome to Nematashi Yameru")

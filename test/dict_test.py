0c9312609ebab89252ccd5fcb661b8389619a33b
Jadon <47489511+gonhunterx@users.noreply.github.com>
2024-01-05 19:21:54 -0500

    update

---
+++ test/dict_test.py
@@ -0,0 +1,10 @@
+ppl = {}
+
+class People:
+    def __init__(self, name, id, job):
+        self.name = name
+        self.id = id 
+        self.job = job 
+        
+    def insert(self, person):
+        pass
--- test/tst.py
+++ test/tst.py
@@ -3,3 +3,29 @@ t = "abc"
 parsed_t = [item for item in t]
 
 print(parsed_t)
+
+
+my_dict = {}
+
+my_dict["state"] = "NY"
+
+
+# return the index of a pair in the index 
+print(my_dict["state"].index("NY"))
+
+# print state
+print(my_dict["state"])
+
+# create a var containing the value of the key
+state = my_dict.get("state")
+print(f"state: {state}")
+
+
+
+print(my_dict)
+
+
+
+
+
+
